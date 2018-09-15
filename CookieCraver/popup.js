5ae3d221b86c046f969343857a6e010f5576f8a1
Jack Biggin <me@jackbiggin.me>
2018-09-15 09:21:45 -0400

    Fixing things

--- CookieCraver/popup.js
+++ CookieCraver/popup.js
@@ -5,12 +5,9 @@ let score = document.getElementById('score');
 // info.innerText = `${user.name ? `${user.name}:` : ''} ${user.score || 0}`;
 chrome.storage.sync.get('sessionUser', (val) => {
   const { sessionUser } = val;
-<<<<<<< HEAD
   info.innerHTML = '<img src="' + `${sessionUser.picture}` +'" class="avatar" />' + `${sessionUser.name}` + "'s cookies";
-=======
   console.log(sessionUser);
   info.innerText = `${sessionUser.name}`;
->>>>>>> extension
 });
 chrome.storage.sync.get('count', (val) => {
   const { count } = val;
