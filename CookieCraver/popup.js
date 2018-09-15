a6ef9a95211e436c66a331ba7fc3fc9460dde640
izyb <isaacbeckie@gmail.com>
2018-09-15 13:25:31 -0700

    remove commented code

--- CookieCraver/popup.js
+++ CookieCraver/popup.js
@@ -1,8 +1,6 @@
 let info = document.getElementById('info');
 let score = document.getElementById('score');
 
-// fetch('USERS_ENDPOINT').then((user) => {
-// info.innerText = `${user.name ? `${user.name}:` : ''} ${user.score || 0}`;
 chrome.storage.sync.get('sessionUser', (val) => {
   const { sessionUser } = val;
   info.innerHTML = `<img src=${sessionUser.picture} class="avatar" /> ${sessionUser.name}'s cookies`;
@@ -14,5 +12,3 @@ chrome.storage.sync.get('count', (val) => {
     .split('')
     .forEach(digit => score.innerHTML += `<span>${digit}</span>`);
 });
-
-// });
