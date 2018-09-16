bfd1af1860004d15f52191dd4b05a97174b815b6
izyb <isaacbeckie@gmail.com>
2018-09-15 18:25:51 -0700

    add quick hiding of chrome extension download button

--- CookieCraver/onWebPage.js
+++ CookieCraver/onWebPage.js
@@ -1,10 +1,10 @@
 const API_URL = 'http://cookiecraver.eastus.cloudapp.azure.com';
+document.getElementById('card').style.display = "block";
 chrome.storage.sync.get('sessionUser', (val) => {
   const { sessionUser } = val;
   fetch(`${API_URL}/user/cookies?uid=${sessionUser.uid}`)
     .then(data => data.json())
     .then((data) => {
-      document.getElementById('card').style.display = "block";
       document.getElementById('download').style.display = "none";
       const playerPic = document.getElementById('player-pic');
       const playerRank = document.getElementById('player-rank');
